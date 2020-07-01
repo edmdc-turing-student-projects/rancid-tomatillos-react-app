bd643ad5b139b1beeb07e776c201f1f064a87082
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-06-30 21:33:36 -0600

    "Add initial styles to homepage display"

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import './Movie.css';
+import React from "react";
+import "./Movie.css";
 
-const Movie = ({id, posterPath, backdropPath, title, averageRating, releaseDate}) => {
-  
-    return (
-    <section>
-      <ul>
-        <li>{title}</li>
-        <li>{averageRating}</li>
-        <li>{id}</li>
-        <li>{posterPath}</li>
-        <li>{backdropPath}</li>
-        <li>{releaseDate}</li>
-      </ul>
-    </section>
-  )
-}
+const Movie = ({ posterPath, title, averageRating, releaseDate }) => {
+  const formatDate = (releaseDate) => {
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const formattedDate = new Date(releaseDate).toLocaleString(
+      "en-US",
+      options
+    );
+    return `Released on ${formattedDate}`;
+  };
+  return (
+    <article>
+      <h4>{title}</h4>
+      <p>{formatDate(releaseDate)}</p>
+      <img alt={`${title} movie poster`} src={posterPath} />
+      <li>{averageRating}</li>
+    </article>
+  );
+};
 
 export default Movie;
+
