319166b010d2e1a2d403bea2561c18c5f38dadbf
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-07-03 11:39:51 -0600

    "Refactor fetch calls to use async and await"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Movies from "./Components/Movies/Movies";
 import LogIn from "./Components/LogIn/LogIn";
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 class App extends Component {
   constructor() {
@@ -17,50 +17,55 @@ class App extends Component {
 
   componentDidMount() {
     const requestUrl = `${this.url}/movies`;
-    return fetch(requestUrl)
-      .then((response) => {
+    const getMoviesRequest = async () => {
+      try {
+        const response = await fetch(requestUrl);
+        let movies;
+
         if (response.ok) {
-          return response.json();
+          movies = await response.json();
+          this.setState({ movies: movies.movies, error: "" });
         } else {
           throw new Error({ ...response });
         }
-      })
-      .then((movies) => {
-        this.setState({ movies: movies.movies, error: "" });
-      })
-      .catch(error => this.setState({error: error}))
+      } catch (error) {
+        this.setState({ error: error });
+      }
+    };
+    return getMoviesRequest();
   }
 
-  postUser = (userInfo) => {
+  postUser = (userCredentials) => {
     const requestUrl = `${this.url}/login`;
-    return fetch(requestUrl, {
+    const userLoginCredentials = {
       method: "POST",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify(userInfo)
-    })
-      .then((response) => {
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(userCredentials),
+    };
+    const loginUser = async () => {
+      try {
+        const response = await fetch(requestUrl, userLoginCredentials);
+        let userInfo;
         if (response.ok) {
-          return response.json()
+          userInfo = await response.json();
+          this.setState({ user: userInfo.user });
         } else {
-          throw new Error ({...response})
+          throw new Error({ ...response });
         }
-      })
-      .then(user => {
-        this.setState({user: user.user})
-        // alert('Successful Login!')
-      })
-      .catch(error => this.setState({error: error}))
-  }
+      } catch (error) {
+        this.setState({ error: error });
+      }
+    };
+    return loginUser();
+  };
 
   logOut(event) {
     event.preventDefault();
-    if(event.target.innerHTML === 'Log Out') {
+    if (event.target.innerHTML === "Log Out") {
       this.setState({
-        user: {}
+        user: {},
       });
-    };
+    }
   }
 
   render() {
@@ -68,19 +73,23 @@ class App extends Component {
       <section>
         <h2> Rancid Tomatillos </h2>
         <nav>
-          <button onClick={event => this.logOut(event)}>          
-            {!this.state.user.email && <Link to="/login" className="nav">Log In</Link> }
+          <button onClick={(event) => this.logOut(event)}>
+            {!this.state.user.email && (
+              <Link to="/login" className="nav">
+                Log In
+              </Link>
+            )}
             {this.state.user.email && "Log Out"}
           </button>
         </nav>
-          <Switch>
-            <Route exact path="/">
-              {this.state.movies && <Movies movies={this.state.movies} />}
-            </Route>
-            <Route exact path="/login">
-              <LogIn postUser={this.postUser} />
-            </Route>
-          </Switch>
+        <Switch>
+          <Route exact path="/">
+            {this.state.movies && <Movies movies={this.state.movies} />}
+          </Route>
+          <Route exact path="/login">
+            <LogIn postUser={this.postUser} />
+          </Route>
+        </Switch>
       </section>
     );
   }
