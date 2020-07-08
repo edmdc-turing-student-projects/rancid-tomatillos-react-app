dd4524211bf6d746c7ed507e7aabb2c91e2d1d0b
Alex Sodemann <asodemann18@gmail.com>
2020-07-07 19:59:54 -0600

    Create UserReview component and apiCall setup

---
+++ src/Components/UserReviews/UserReviews.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { getReviews } from '/Users/alexandriasodemann/Desktop/turingPairProject3/rancid-tomatillos-react-app/src/apiCalls.js'
+import { getReviewsResponse } from '/Users/alexandriasodemann/Desktop/turingPairProject3/rancid-tomatillos-react-app/src/apiCalls.js'
+ 
+const UserReviews = ({ userId }) => {
+  // getReviews = () => {
+
+  // }  
+
+  return (
+    <p>{userId}</p>
+  );
+}
+
+export default UserReviews;
