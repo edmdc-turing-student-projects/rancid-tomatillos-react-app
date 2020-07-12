9a9eef400bd35fec0bbe4b583299556b96c4c859
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-07-12 14:40:39 -0600

    "Add movies component unit test checks for single movie"

--- src/App.test.js
+++ src/App.test.js
@@ -11,10 +11,10 @@ getAllMovies.mockResolvedValue({
     {
       average_rating: 6.333333333333333,
       backdrop_path:
-      "https://image.tmdb.org/t/p/original//o0F8xAt8YuEm5mEZviX5pEFC12y.jpg",
+        "https://image.tmdb.org/t/p/original//o0F8xAt8YuEm5mEZviX5pEFC12y.jpg",
       id: 475430,
       poster_path:
-      "https://image.tmdb.org/t/p/original//tI8ocADh22GtQFV28vGHaBZVb0U.jpg",
+        "https://image.tmdb.org/t/p/original//tI8ocADh22GtQFV28vGHaBZVb0U.jpg",
       release_date: "2020-06-12",
       title: "Artemis Fowl",
     },
--- src/Components/Movies/Movies.test.js
+++ src/Components/Movies/Movies.test.js
@@ -1,4 +1,38 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Movies from './Movies';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+import {MemoryRouter} from 'react-router-dom'
+
+const movie1 = {
+  average_rating: 6.3,
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original//o0F8xAt8YuEm5mEZviX5pEFC12y.jpg",
+  id: 475430,
+  poster_path:
+    "https://image.tmdb.org/t/p/original//tI8ocADh22GtQFV28vGHaBZVb0U.jpg",
+  release_date: "2020-06-12",
+  title: "Artemis Fowl",
+}
+
+const movie2 = {
+  average_rating: 3.3,
+  backdrop_path:"https://image.tmdb.org/t/p/original//o0F8xAt8YuEm5mEZviX5pEFC12y.jpg",
+  id: 338762,
+  poster_path:"https://image.tmdb.org/t/p/original//8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg",
+  release_date: "2020-03-05",
+  title: "Bloodshot",
+};
+
+describe("Movies without user logged in", () => {
+  it('should render a movie card', () => {
+    const {getByRole} = render(
+      <MemoryRouter>
+        <Movies movies={[movie1]}/>
+      </MemoryRouter>
+    );
+
+    const moviePoster = getByRole('img', {name:`${movie1.title} movie poster`} )
+    expect(moviePoster).toBeInTheDocument();
+  })
+});
